ec8109a40a68e7b3e4e015a7a418c47ca4803b74
fitzpaky <kfitzpatrick44@gmail.com>
2020-04-20 16:17:22 -0700

    adding pseudocode for friend match

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -16,11 +16,11 @@ app.get("/api/friends", function(req, res) {
 
 // Displays a single friend, or returns false
 app.get("/api/friends/:friend", function(req, res) {
-  var chosen = req.params.friend;
+  var chosen = req.params.friendData;
 
   console.log(chosen);
 
-  for (var i = 0; i < friendData.length; i++) {
+  for (var i = 0; i < friendsData.length; i++) {
     if (chosen === friendsData[i].routeName) {
       return res.json(friendsData[i]);
     }
@@ -33,17 +33,20 @@ app.get("/api/friends/:friend", function(req, res) {
 app.post("/api/friends", function(req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
-  var newFriend = req.body;
+  
 
   // Using a RegEx Pattern to remove spaces from newFriend
   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newFriend = newFriend.name.replace(/\s+/g, "").toLowerCase();
 
-  console.log(newFriend);
+//scoresFriend = req.body.scores.map(Number);
+//convert scores to int
+//match friends based on closest scores
+//loop through score and find score with the smallest difference
 
-  friendsData.push(newFriend);
+  friendsData.push(req.body)
+  res.json(true);
+  console.log(req.body);
 
-  res.json(newFriend);
 });
 }
 
