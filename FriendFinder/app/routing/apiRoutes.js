d58eed163d26b5cbd5aadfb415377545a3e11b87
fitzpaky <kfitzpatrick44@gmail.com>
2020-04-20 21:09:57 -0700

    adding for loop for friends match

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -42,9 +42,27 @@ app.post("/api/friends", function(req, res) {
 //convert scores to int
 //match friends based on closest scores
 //loop through score and find score with the smallest difference
+var totalDifference = 0
+//for loop for friends array
+for(var i = 0; i < friendsData.length; i++){
+    for(var j = 0; j < friendsData[i].scores.length; j++){
+        req.body.scores[j] = parseInt(req.body.scores[j])
+        totalDifference = totalDifference + Math.abs((friendsData[i].scores[j] - req.body.scores[j]))
+    }
+    friendsData[i].totalDifference = totalDifference
+}
 
+
+friendsData.sort(function(a,b){
+    return a.totalDifference - b.totalDifference;
+})
+
+console.log(friendsData)
+//for loop for scores
+//difference between friends array score and user score and absolute value of difference then add absolute difference together
+//sort the absolute difference and chose the friend with least difference 
   friendsData.push(req.body)
-  res.json(true);
+  res.json(friendsData[0]);
   console.log(req.body);
 
 });
