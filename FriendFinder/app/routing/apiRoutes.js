4589b359e3ddbfb0dad538d417cdf4bea5ab0e22
fitzpaky <kfitzpatrick44@gmail.com>
2020-04-24 11:01:15 -0700

    adding html modal display

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -1,13 +1,6 @@
-// Dependencies
-// =============================================================
+// dependencies
 var friendsData = require("../data/friend.js")
 
-// // Sets up the Express App
-// // =============================================================
-
-
-// Routes
-// =============================================================
 module.exports = function(app){
 // Displays all Friends
 app.get("/api/friends", function(req, res) {
@@ -16,7 +9,7 @@ app.get("/api/friends", function(req, res) {
 app.get("https://github.com/KylaFitzpatrick/Friend-Finder-Node-and-Express-Servers", function(req, res) {
     res.json();
   });
-// Displays a single friend, or returns false
+// displays a single friend, or returns false
 app.get("/api/friends/:friend", function(req, res) {
   var chosen = req.params.friendData;
 
@@ -31,20 +24,12 @@ app.get("/api/friends/:friend", function(req, res) {
   return res.json(false);
 });
 
-// Create New Friends - takes in JSON input
+// create New Friends - takes in JSON input
 app.post("/api/friends", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  
 
-  // Using a RegEx Pattern to remove spaces from newFriend
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-
-//scoresFriend = req.body.scores.map(Number);
 //convert scores to int
 //match friends based on closest scores
 //loop through score and find score with the smallest difference
-// var totalDifference = 0
 //for loop for friends array
 for(var i = 0; i < friendsData.length; i++){
     var totalDifference = 0
--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -1,19 +1,19 @@
 var bodyParser = require("body-parser");
 var express = require("express");
-var path = require("path");
+// var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
+// sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-// Starts the server to begin listening
-// =============================================================
+// starts the server to begin listening
+
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
